6c8e352b38282bd3d517d823af711086e31ddbad
Evan St Martin <evanstm011@gmail.com>
2021-01-26 21:34:49 -0600

    added prompt functions. Runs in integrated terminal

--- Develop/index.js
+++ Develop/index.js
@@ -1,62 +1,65 @@
-// // TODO: Include packages needed for this application
-
-// // TODO: Create an array of questions for user input
-// const questions = [];
-
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-
-// // TODO: Create a function to initialize app
-// function init() {}
-
-// // Function call to initialize app
-// init()
+//calling packages to run application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const Choices = require('inquirer/lib/objects/choices');
-const markdown = require('./generateMarkdown.js');
+const generate = require('./utils/generateMarkdown');
 
-inquirer.prompt([
-    {
-        type: "input",
-        message: "What is the title of your project?",
-        name: "title"
-    },
-    {
-        type: "input",
-        message: "please describe your project.",
-        name: "description"
-    },
-    {
-        type: 'input',
-        message: 'Enter the installation instructions for your project',
-        name: 'installation',
-    },
-    {
-        type: "list",
-        message: "What type of license do you want?",
-        name: "license",
-        choices: ["MIT License",
-            "Apache License 2.0",
-            "GPL License",
-            "BSD License"]
-    },
-    {
-        type: 'input',
-        message: 'Enter your Github username',
-        name: 'username'
-    },
-    {
-        type: 'input',
-        message: 'Enter your email',
-        name: 'email'
-    },
-]
-    
-    // fs.writeFile('README.md', data, (err) =>
-    // err ? console.error(err) : console.log('Success!')
-    // )
+//array of questions
+const questions =
+    [
+        {
+            type: "input",
+            message: "What is the title of your project?",
+            name: "title",
+        },
+        {
+            type: "input",
+            message: "Please describe your project:",
+            name: "description",
+        },
+        {
+            type: 'input',
+            message: 'Enter the installation instructions for your project:',
+            name: 'installation',
+        },
+        {
+            type: "input",
+            message: "Please enter link to the deployed application:",
+            name: "link",
+        },
+        {
+            type: "list",
+            message: "What type of license do you want?",
+            name: "license",
+            choices: ["MIT License",
+                "Apache License 2.0",
+                "GPL License",
+                "BSD License",
+                "None"]
+        },
+        {
+            type: 'input',
+            message: 'Enter your Github username',
+            name: 'username',
+        },
+        {
+            type: 'input',
+            message: 'Enter your email',
+            name: 'email',
+        },
+    ];
+
+//promt user function
+inquirer
+    .prompt(questions)
+    .then((response) => writeToFile(response));
+
+//create Readme file function
+function writeToFile(fileName, data) {
+    fs.writeFile("testREADME.md", generate("data"), err => err
+        ? console.error(err)
+        : console.log("Success! Your ReadMe.md is being generated."));
+
+};
 
 
 
-);
